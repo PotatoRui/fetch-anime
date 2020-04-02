42a2def1473d7cda15b4a6cc70557d1c179d48a5
rangigo <panigo3797@gmail.com>
2020-04-02 23:07:07 +0300

    Add Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 import Navigation from './components/Navigation'
 import List from './containers/List'
 import styles from './App.module.scss'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faCaretDown,
-  faArrowLeft,
-  faArrowRight,
-} from '@fortawesome/free-solid-svg-icons'
-
-library.add(faCaretDown, faArrowLeft, faArrowRight)
-
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <Navigation />
-        <div className={styles.Content}>
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="/list/2020/spring" />
-            </Route>
-            <Route path="/list/genres/:genre/:type/:page" component={List} />
-            <Route path="/list/:year/:season" component={List} />
-            <Route path="*">
-              <Redirect to="/list/2020/spring" />
-            </Route>
-          </Switch>
-        </div>
+function App() {
+  return (
+    <div className={styles.App}>
+      <Navigation />
+      <div className={styles.Content}>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/list/2020/spring" />
+          </Route>
+          <Route path="/list/genres/:genre/:type/:page" component={List} />
+          <Route path="/list/:year/:season" component={List} />
+          <Route path="*">
+            <Redirect to="/list/2020/spring" />
+          </Route>
+        </Switch>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App
